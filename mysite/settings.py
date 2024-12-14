742b479d016d67bcbc44636cbc9a75b5b2967598
SergeyFoma <asdfg2105c@gmail.com>
2024-12-14 09:29:09 +0500

    settings update

--- mysite/settings.py
+++ mysite/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'apps.shop.ShopConfig',
+    'shop.apps.ShopConfig',
 ]
 
 MIDDLEWARE = [
