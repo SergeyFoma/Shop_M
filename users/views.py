746e900188298bf724a2484ea75366bc158aa0d5
SergeyFoma <asdfg2105c@gmail.com>
2025-01-24 15:06:36 +0500

    update__asd

--- users/views.py
+++ users/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render
+from users.forms import UserLoginForm
+from django.contrib import auth
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 def login(request):
+    if request.method == 'POST':
+        form=UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username=request.POST['username']
+            password=request.POST['password']
+            user=auth.authenticate(username=username, password=password)
+            if user:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse('shop:index'))
+    else:
+        form=UserLoginForm()
     context={
-
+        'form':form,
     }
     return render(request, "users/login.html", context)
 
